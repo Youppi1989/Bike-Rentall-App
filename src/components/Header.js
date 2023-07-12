b923e317f92162750f1bdffd91df8d18feaf6d11
Your Username <trotsenko_nika@mail.ru>
2023-07-12 15:22:57 +0200

    final

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,35 +7,39 @@ import logoImage from "../assets/IMG_9780 (1).PNG";
 function Header() {
   const { store } = useContext(Context);
   return (
-    <header class={styles.header}>
-      <img class={styles.logo} src={logoImage} alt="Logo" />
+    <header className={styles.header}>
+      <img className={styles.logo} src={logoImage} alt="Logo" />
       <Link to="/">
-        <button class="button_main">Главная страница</button>
+        <button className="button_main">Главная страница</button>
       </Link>
       {store.isAuth ? (
         <>
-          <button disabled class="button-outline">
+          <button disabled className="button-outline">
             {store.user.email}
           </button>
           <Link to="/">
-            <button onClick={() => store.logout()} type="button" class="button">
+            <button
+              onClick={() => store.logout()}
+              type="button"
+              className="button_enter"
+            >
               Выйти
             </button>
           </Link>
         </>
       ) : (
-        <>
+        <div className={styles.authButtons}>
           <Link to="/login">
-            <button type="button" class="button">
+            <button type="button" className="button">
               Вход
             </button>
           </Link>
           <Link to="/registration">
-            <button type="button" class="button">
+            <button type="button" className="button">
               Регистрация
             </button>
           </Link>
-        </>
+        </div>
       )}
     </header>
   );
